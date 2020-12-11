386fadd7bb2e36d2aa5834ca05994793e053ec56
Osvaldo Medina <OsvaldoMDLM15@users.github.com>
2020-12-10 18:46:33 -0600

    commit

--- controller/CRUD.php
+++ controller/CRUD.php
@@ -10,7 +10,7 @@ if(isset($_POST['crearUsuario'])){
     }
 }
 
-if(isset($_POST['id'])){
+if(isset($_POST['actualizadatos'])){
     $id = $_POST['id'];
     $nombre = $_POST['nombre'];
     $correo = $_POST['correo'];
