42b15dfd852399dc24e672febb7a81633eeda968
Osvaldo Medina <OsvaldoMDLM15@users.github.com>
2020-12-10 22:59:46 -0600

    ModalOff

--- view/edituser.php
+++ view/edituser.php
@@ -18,12 +18,12 @@
         <div class="columns is-centered">
             <div class="column is-three-fifths is-offset-one-fifth">
                 <h1 class="title is-2">Editar usuario</h1>
-                <form name="frmdatos" action="../controller/CRUD.php?id=<?php echo $_GET['id'];?>" method="POST">
+                <form name="frmdatos" action="../controller/CRUD.php?id=<?php echo $_POST['id'];?>" method="POST">
                     <div class="field">
                         <label class="label">Nombre</label>
                         <div class="control">
                             <div class="columns">
-                                <div class="column is-three-fifths"><input class="input" type="text" id="nombre" name="nombre" placeholder="e.g Alex Smith" value="<?php echo$name?>"></div>
+                                <div class="column is-three-fifths"><input class="input" type="text" id="nombre" name="nombre" placeholder="e.g Alex Smith" value="<?php echo$nombre?>"></div>
                             </div>
                         </div>
                     </div>
@@ -43,13 +43,14 @@
                         <div class="control">
                             <div class="columns">
                                 <div class="column is-two-fifths">
-                                    <input class="input" type="text" id="login" name="login" placeholder="e.g. AlexS" value="<?php echo$loginU?>"></div>
+                                    <input class="input" type="text" id="login" name="login" placeholder="e.g. AlexS" value="<?php echo$login?>"></div>
                             </div>
                         </div>
                     </div>
+                    <input class="input" type="hidden" id="login" name="login" value="<?php echo$id?>"></div>
 
                     <div class="control">
-                        <button id="enviar" name="editar" class="button is-success">&nbsp;Actualizar datos&nbsp;</button>
+                        <button id="editar" name="editar" class="button is-success">&nbsp;Actualizar datos&nbsp;</button>
                     </div>
                 </form>
 
